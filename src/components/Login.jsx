23ea5fc91f2244a89393872332e8c47e62e12c9a
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-08-02 13:54:03 +0100

    delete unnecessary curly braces

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,8 +4,8 @@ import Input from "./Input";
 function Login() {
   return (
     <form className="form">
-      <Input inputType={"text"} inputPlaceholder={"Username"} />
-      <Input inputType={"password"} inputPlaceholder={"password"} />
+      <Input inputType="text" inputPlaceholder="Username" />
+      <Input inputType="password" inputPlaceholder="password" />
       <button type="submit">Login</button>
     </form>
   );
