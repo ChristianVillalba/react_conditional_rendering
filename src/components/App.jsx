f3e4dac2d7e502a724fc981369c80f0c35b63bb8
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-08-03 15:57:28 +0100

    ternary operator working only on certain condition. return null otherwise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Login from "./Login";
+// import Login from "./Login";
 
-var isLoggedIn = false;
-
+// var isLoggedIn = false;
+// function App() {
+//   return <div className="container">{isLoggedIn ? <h1>Hello</h1> : <h1>Hello</h1>}</div>;
+// }
+const currentTime = new Date().getHours();
+console.log(currentTime);
 function App() {
   return (
     <div className="container">
-      {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+      {currentTime > 12 ? <h1>Why are you still working</h1> : null}
     </div>
   );
 }
